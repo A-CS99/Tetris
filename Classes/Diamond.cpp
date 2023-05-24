5d82b122ccb880f4f869a9e667e67f45a3794250
Jiaming He <117623136+A-CS99@users.noreply.github.com>
2023-05-24 13:11:27 +0800

    Delete Classes directory

--- Classes/Diamond.cpp
+++
@@ -1,7 +0,0 @@
-#include "Diamond.h"
-
-bool Diamond::init() {
-	initWithFile("block_new.png");
-
-	return true;
-}
--- Classes/MyMenu.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<iostream>
-#include"cocos2d.h"
-
-class MyMenu : public cocos2d::Menu {
-public:
-	virtual bool init();
-	CREATE_FUNC(MyMenu);
-};
